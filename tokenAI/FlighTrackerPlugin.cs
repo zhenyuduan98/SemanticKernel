399504ffcf2a3c3418e062faaada94d10869d54c
zhenyuduan <zhenyuduan@ZHENYUDUAN>
2024-12-29 17:24:46 +0800

    Add project files.

---
+++ tokenAI/FlighTrackerPlugin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCopilot.Plugins.FlightTrackerPlugin
+{
+    public class FlightTrackerPlugin(string apiKey)
+    {
+        readonly HttpClient client = new HttpClient();
+
+        [KernelFunction, Description("Tracks the flight status of a provided source and destination")]
+        [return: Description("Flight details and status")]
+        public async Task<string> TrackFlightAsync(
+        [Description("IATA code for the source location")] string source,
+        [Description("IATA code for the designation location")] string destination,
+        [Description("IATA code for the flight")] string flightNumber,
+        [Description("Count of flights")] int limit)
+        {
+            //string apiKey = "cf62b704fb573a7dbe90976ffefb9a46"; 
+            //string source = "JFK";         
+            //string destination = "LAX";    
+            //int limit = 10;                
+            //string flightNumber = "AA100"; 
+            string url = $"http://api.aviationstack.com/v1/flights?access_key={apiKey}&dep_iata={source}&arr_iata={destination}&limit={limit}&flight_iata={flightNumber}";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+
+    }
+}
---
+++ tokenAI/Program.cs
@@ -0,0 +1,77 @@
+﻿using CustomCopilot.Plugins.FlightTrackerPlugin;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using static System.Environment;
+
+namespace CustomCopilot
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+
+        {
+            Environment.SetEnvironmentVariable("AOI_ENDPOINT_SWDN", "https://azureopenaizhenyu.openai.azure.com");
+            Environment.SetEnvironmentVariable("AOI_KEY_SWDN", "c38cc27ce9914aaeb84cb71bc88116d9");
+
+      
+            var builder = Kernel.CreateBuilder();
+            builder.AddAzureOpenAIChatCompletion("gpt-4o",
+               GetEnvironmentVariable("AOI_ENDPOINT_SWDN")!,
+                GetEnvironmentVariable("AOI_KEY_SWDN")!);
+
+            builder.Plugins.AddFromObject(new FlightTrackerPlugin("cf62b704fb573a7dbe90976ffefb9a46"), nameof(FlightTrackerPlugin));
+        
+
+            var kernel = builder.Build();
+
+            ChatHistory history = [];
+            history.AddSystemMessage(@"You're a virtual assistant that helps people track flight and find information.");
+
+        
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+       
+            while (true)
+            {
+              
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("User > ");
+                history.AddUserMessage(Console.ReadLine()!);
+
+                OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+                {
+                    MaxTokens = 200
+                };
+
+                
+
+                var response = chatCompletionService.GetStreamingChatMessageContentsAsync(
+                               history,
+                               executionSettings: openAIPromptExecutionSettings,
+                               kernel: kernel);
+
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\nAssistant > ");
+
+                string combinedResponse = string.Empty;
+                await foreach (var message in response)
+                {
+                   
+
+                    Console.Write(message);
+                    combinedResponse += message;
+                }
+
+                Console.WriteLine();
+
+            
+
+                history.AddAssistantMessage(combinedResponse);
+            }
+        }
+    }
+}
